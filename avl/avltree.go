005ea5e2a65aa14239c365770e7999c27e690365
wuyu <wuyu@yunify.com>
2020-12-12 21:57:02 +0800

    Add package avl that moved from gohelper

---
+++ avl/avltree.go
@@ -0,0 +1,282 @@
+// Copyright (c) 2020, Yu Wu <yu.771991@gmail.com> All rights reserved.
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package avl
+
+import (
+	"fmt"
+
+	"github.com/yu31/gostructs/bs"
+	"github.com/yu31/gostructs/container"
+)
+
+// Type aliases for simplifying use in this package.
+type Key = container.Key
+type Value = container.Value
+type Element = container.Element
+type TreeNode = bs.TreeNode
+
+// treeNode is used for avl tree.
+//
+// And it is also the implementation of interface container.Element and bs.TreeNode
+type treeNode struct {
+	key    Key
+	value  Value
+	left   *treeNode
+	right  *treeNode
+	height int
+}
+
+// Key returns the key.
+func (n *treeNode) Key() Key {
+	return n.key
+}
+
+// Value returns the value.
+func (n *treeNode) Value() Value {
+	return n.value
+}
+
+// Left returns the left child of the TreeNode.
+func (n *treeNode) Left() TreeNode {
+	return n.left
+}
+
+// Right returns the right child of the TreeNode.
+func (n *treeNode) Right() TreeNode {
+	return n.right
+}
+
+// Tree implements the AVL Tree.
+//
+// And it is also the implementation of interface container.Container
+type Tree struct {
+	root *treeNode
+	len  int
+}
+
+// New creates an AVL Tree.
+func New() *Tree {
+	tr := &Tree{
+		root: nil,
+		len:  0,
+	}
+	return tr
+}
+
+// Len returns the number of elements.
+func (tr *Tree) Len() int {
+	return tr.len
+}
+
+// Insert inserts the giving key and value as an Element and returns.
+// Returns nil if key already exists.
+func (tr *Tree) Insert(k Key, v Value) Element {
+	var n *treeNode
+	tr.root, n = tr.insert(tr.root, k, v)
+	if n == nil {
+		return nil
+	}
+	tr.len++
+	return n
+}
+
+// Delete removes and returns the Element of a given key.
+// Returns nil if not found.
+func (tr *Tree) Delete(k Key) Element {
+	var d *treeNode
+	tr.root, d = tr.delete(tr.root, k)
+	if d == nil {
+		return nil
+	}
+
+	d.left = nil
+	d.right = nil
+	d.height = -1
+
+	tr.len--
+	return d
+}
+
+// Search returns the Element of a given key.
+// Returns nil if not found.
+func (tr *Tree) Search(k Key) Element {
+	p := tr.root
+	for p != nil {
+		flag := k.Compare(p.key)
+		if flag == -1 {
+			p = p.left
+		} else if flag == 1 {
+			p = p.right
+		} else {
+			return p
+		}
+	}
+	return nil
+}
+
+// Iter return an Iterator, it's a wrap for bs.Iterator.
+func (tr *Tree) Iter(start Key, boundary Key) container.Iterator {
+	return bs.NewIterator(tr.root, start, boundary)
+}
+
+func (tr *Tree) swap(n1, n2 *treeNode) {
+	n1.key, n2.key = n2.key, n1.key
+	n1.value, n2.value = n2.value, n1.value
+}
+
+// return (root root, new node)
+func (tr *Tree) insert(root *treeNode, k Key, v Value) (*treeNode, *treeNode) {
+	var n *treeNode
+
+	if root == nil {
+		n = tr.createNode(k, v)
+		root = n
+	} else {
+		flag := k.Compare(root.key)
+		if flag == -1 {
+			// Insert into the left subtree.
+			root.left, n = tr.insert(root.left, k, v)
+		} else if flag == 1 {
+			// Insert into the right subtree
+			root.right, n = tr.insert(root.right, k, v)
+		} else {
+			// The key already exists. Not allow duplicates.
+			return root, nil
+		}
+		if n != nil {
+			root = tr.reBalance(root)
+		}
+	}
+
+	return root, n
+}
+
+// return (root root, delete node).
+func (tr *Tree) delete(root *treeNode, k Key) (*treeNode, *treeNode) {
+	var d *treeNode
+	if root == nil {
+		// not found
+		return nil, nil
+	} else {
+		flag := k.Compare(root.key)
+		if flag == -1 {
+			// delete from the left subtree.
+			root.left, d = tr.delete(root.left, k)
+		} else if flag == 1 {
+			// delete from the right subtree.
+			root.right, d = tr.delete(root.right, k)
+		} else {
+			if root.left != nil && root.right != nil {
+				if tr.nodeHeight(root.left) > tr.nodeHeight(root.right) {
+					x := root.left
+					for x.right != nil {
+						x = x.right
+					}
+					tr.swap(root, x)
+					root.left, d = tr.delete(root.left, k)
+				} else {
+					x := root.right
+					for x.left != nil {
+						x = x.left
+					}
+					tr.swap(root, x)
+					root.right, d = tr.delete(root.right, k)
+				}
+			} else {
+				d = root
+				if d.left != nil {
+					root = d.left
+				} else {
+					root = d.right
+				}
+			}
+		}
+		if root != nil {
+			root = tr.reBalance(root)
+		}
+	}
+
+	return root, d
+}
+
+func (tr *Tree) reBalance(n *treeNode) *treeNode {
+	if n == nil {
+		return nil
+	}
+
+	factor := tr.nodeHeight(n.left) - tr.nodeHeight(n.right)
+
+	switch factor {
+	case -1, 0, 1:
+		n.height = tr.calculateHeight(n)
+	case 2:
+		// Left subtree higher than right subtree.
+		if tr.nodeHeight(n.left.right) > tr.nodeHeight(n.left.left) {
+			n.left = tr.leftRotate(n.left)
+		}
+		n = tr.rightRotate(n)
+	case -2:
+		// Left subtree lower than right subtree.
+		if tr.nodeHeight(n.right.left) > tr.nodeHeight(n.right.right) {
+			n.right = tr.rightRotate(n.right)
+		}
+		n = tr.leftRotate(n)
+	default:
+		panic(fmt.Errorf("avl: unexpected cases with invalid factor <%d>", factor))
+	}
+
+	return n
+}
+
+func (tr *Tree) createNode(k Key, v Value) *treeNode {
+	return &treeNode{
+		key:    k,
+		value:  v,
+		left:   nil,
+		right:  nil,
+		height: 1,
+	}
+}
+
+func (tr *Tree) nodeHeight(n *treeNode) int {
+	if n == nil {
+		return 0
+	}
+	return n.height
+}
+
+func (tr *Tree) calculateHeight(n *treeNode) int {
+	lh := tr.nodeHeight(n.left)
+	rh := tr.nodeHeight(n.right)
+	if lh > rh {
+		return lh + 1
+	}
+	return rh + 1
+}
+
+func (tr *Tree) leftRotate(n *treeNode) *treeNode {
+	r := n.right
+
+	n.right = r.left
+	r.left = n
+
+	n.height = tr.calculateHeight(n)
+	r.height = tr.calculateHeight(r)
+
+	return r
+}
+
+func (tr *Tree) rightRotate(n *treeNode) *treeNode {
+	l := n.left
+
+	n.left = l.right
+	l.right = n
+
+	n.height = tr.calculateHeight(n)
+	l.height = tr.calculateHeight(l)
+
+	return l
+}
---
+++ avl/avltree_test.go
@@ -0,0 +1,226 @@
+package avl
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/yu31/gostructs/bs"
+	"github.com/yu31/gostructs/container"
+)
+
+func recurseCalculateNodeHeight(n *treeNode) int {
+	if n == nil {
+		return 0
+	}
+	lh := recurseCalculateNodeHeight(n.left)
+	rh := recurseCalculateNodeHeight(n.right)
+	if lh > rh {
+		return lh + 1
+	}
+	return rh + 1
+}
+
+func checkBalance(t *testing.T, tr *Tree, n *treeNode) {
+	if n == nil {
+		return
+	}
+
+	checkBalance(t, tr, n.left)
+	checkBalance(t, tr, n.right)
+
+	// Check the node height calculate.
+	require.Equal(t, tr.nodeHeight(n), recurseCalculateNodeHeight(n))
+
+	if n.left != nil {
+		require.Equal(t, n.key.Compare(n.left.key), 1)
+	}
+	if n.right != nil {
+		require.Equal(t, n.key.Compare(n.right.key), -1)
+	}
+
+	// The height difference cannot exceed 1 in AVL Tree.
+	lh := tr.nodeHeight(n.left)
+	rh := tr.nodeHeight(n.right)
+	if lh > rh {
+		require.Equal(t, lh-rh, 1)
+	} else if lh < rh {
+		require.Equal(t, rh-lh, 1)
+	}
+}
+
+func Test_Interface(t *testing.T) {
+	// Ensure the interface is implemented.
+	var node bs.TreeNode
+	var element container.Element
+	var ct container.Container
+	var it container.Iterator
+
+	node = &treeNode{}
+	_ = node
+	element = &treeNode{}
+	_ = element
+	ct = New()
+	_ = ct
+	it = ct.Iter(nil, nil)
+	_ = it
+}
+
+func TestNew(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr)
+	require.Nil(t, tr.root)
+	require.Equal(t, tr.len, 0)
+}
+
+func TestTree_createNode(t *testing.T) {
+	tr := New()
+
+	k := container.Int64(0xf)
+	v := 1024
+
+	n := tr.createNode(k, v)
+	require.NotNil(t, n)
+	require.Equal(t, n.key.Compare(k), 0)
+	require.Equal(t, n.value, v)
+	require.Nil(t, n.left)
+	require.Nil(t, n.right)
+	require.Equal(t, n.height, 1)
+}
+
+func TestTree_Insert(t *testing.T) {
+	tr := New()
+
+	require.NotNil(t, tr.Insert(container.Int(11), 1024))
+	require.Nil(t, tr.Insert(container.Int(11), 1023))
+	require.NotNil(t, tr.Insert(container.Int(33), nil))
+	require.Nil(t, tr.Insert(container.Int(33), nil))
+	require.NotNil(t, tr.Insert(container.Int(22), nil))
+	require.Nil(t, tr.Insert(container.Int(22), nil))
+}
+
+func TestTree_Delete(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr.Insert(container.Int(11), 1021))
+	require.NotNil(t, tr.Insert(container.Int(22), 1022))
+	require.NotNil(t, tr.Insert(container.Int(33), 1023))
+
+	element := tr.Delete(container.Int(11))
+	require.NotNil(t, element)
+	require.Equal(t, element.Key().Compare(container.Int(11)), 0)
+	require.Equal(t, element.Value(), 1021)
+	require.Nil(t, element.(*treeNode).left)
+	require.Nil(t, element.(*treeNode).right)
+	require.Equal(t, element.(*treeNode).height, -1)
+	require.Nil(t, element.(TreeNode).Left())
+	require.Nil(t, element.(TreeNode).Left())
+	require.Nil(t, tr.Delete(container.Int(11)))
+
+	require.NotNil(t, tr.Delete(container.Int(22)))
+	require.Nil(t, tr.Delete(container.Int(22)))
+	require.NotNil(t, tr.Delete(container.Int(33)))
+	require.Nil(t, tr.Delete(container.Int(33)))
+
+	// Try to delete key not exists.
+	require.Nil(t, tr.Delete(container.Int(1024)))
+}
+
+func TestTree_Search(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr.Insert(container.Int(11), 1021))
+	require.NotNil(t, tr.Insert(container.Int(22), 1022))
+	require.NotNil(t, tr.Insert(container.Int(33), 1023))
+
+	require.Equal(t, tr.Search(container.Int(11)).Key().Compare(container.Int(11)), 0)
+	require.Equal(t, tr.Search(container.Int(11)).Value(), 1021)
+	require.Equal(t, tr.Search(container.Int(22)).Key().Compare(container.Int(22)), 0)
+	require.Equal(t, tr.Search(container.Int(22)).Value(), 1022)
+	require.Equal(t, tr.Search(container.Int(33)).Key().Compare(container.Int(33)), 0)
+	require.Equal(t, tr.Search(container.Int(33)).Value(), 1023)
+
+	// Try to search key not exists.
+	require.Nil(t, tr.Search(container.Int(1024)))
+}
+
+func TestTree_Len(t *testing.T) {
+	tr := New()
+
+	require.NotNil(t, tr.Insert(container.Int(12), 1))
+	require.NotNil(t, tr.Insert(container.Int(18), 1))
+	require.NotNil(t, tr.Insert(container.Int(33), 1))
+
+	// Insert duplicate key.
+	require.Nil(t, tr.Insert(container.Int(12), 1))
+	require.Nil(t, tr.Insert(container.Int(18), 1))
+	require.Nil(t, tr.Insert(container.Int(33), 1))
+
+	require.Equal(t, tr.Len(), 3)
+
+	require.NotNil(t, tr.Delete(container.Int(18)))
+	require.Nil(t, tr.Delete(container.Int(18)))
+
+	// Delete a not exist key.
+	require.Nil(t, tr.Delete(container.Int(1024)))
+
+	require.Equal(t, tr.Len(), 2)
+}
+
+func TestTree(t *testing.T) {
+	tr := New()
+
+	length := 257
+	maxKey := length * 100
+	keys := make([]container.Int, length)
+
+	for x := 0; x < 2; x++ {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		// insert
+		for i := 0; i < length; i++ {
+			for {
+				k := container.Int(r.Intn(maxKey) + 1)
+				if tr.Insert(k, int64(k*2+1)) != nil {
+					require.Nil(t, tr.Insert(k, int64(k*2+1)))
+					keys[i] = k
+					break
+				}
+			}
+			checkBalance(t, tr, tr.root)
+			require.Equal(t, tr.Len(), i+1)
+		}
+
+		require.Equal(t, tr.Len(), length)
+
+		// boundary
+		for _, k := range []container.Int{0, 0xfffffff} {
+			require.NotNil(t, tr.Insert(k, k))
+			require.Nil(t, tr.Insert(k, k))
+			require.NotNil(t, tr.Search(k))
+			require.Equal(t, tr.Search(k).Value(), k)
+			require.NotNil(t, tr.Delete(k))
+			require.Nil(t, tr.Delete(k))
+		}
+
+		// get
+		for i := 0; i < length; i++ {
+			element := tr.Search(keys[i])
+			require.NotNil(t, element)
+			require.Equal(t, element.Key().Compare(keys[i]), 0)
+			require.Equal(t, element.Value(), int64(keys[i]*2+1))
+		}
+
+		// delete
+		for i := 0; i < length; i++ {
+			require.NotNil(t, tr.Delete(keys[i]))
+			require.Nil(t, tr.Delete(keys[i]))
+			require.Nil(t, tr.Search(keys[i]))
+
+			checkBalance(t, tr, tr.root)
+			require.Equal(t, tr.Len(), length-i-1)
+		}
+
+		require.Nil(t, tr.root)
+		require.Equal(t, tr.Len(), 0)
+	}
+}
