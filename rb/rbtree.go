a0513464e662d0f3dcfb04c65cad9f9e526f1185
wuyu <wuyu@yunify.com>
2020-12-12 21:57:15 +0800

    Add package rb that moved from gohelper

---
+++ rb/rbtree.go
@@ -0,0 +1,368 @@
+// Copyright (c) 2020, Yu Wu <yu.771991@gmail.com> All rights reserved.
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package rb
+
+import (
+	"github.com/yu31/gostructs/bs"
+	"github.com/yu31/gostructs/container"
+)
+
+const (
+	red int8 = iota
+	black
+)
+
+// Type aliases for simplifying use in this package.
+type Key = container.Key
+type Value = container.Value
+type Element = container.Element
+type TreeNode = bs.TreeNode
+
+// treeNode is used for Red-Black Tree.
+//
+// And it is also the implementation of interface container.Element and bs.TreeNode
+type treeNode struct {
+	key    Key
+	value  Value
+	left   *treeNode
+	right  *treeNode
+	parent *treeNode
+	color  int8
+}
+
+// Key returns the key.
+func (n *treeNode) Key() Key {
+	return n.key
+}
+
+// Value returns the value.
+func (n *treeNode) Value() Value {
+	return n.value
+}
+
+// Left returns the left child of the TreeNode.
+func (n *treeNode) Left() TreeNode {
+	return n.left
+}
+
+// Right returns the right child of the TreeNode.
+func (n *treeNode) Right() TreeNode {
+	return n.right
+}
+
+// Tree implements the Red-Black Tree.
+//
+// And it is also the implementation of interface container.Container
+type Tree struct {
+	root *treeNode
+	len  int
+}
+
+// New creates an Red-Black Tree.
+func New() *Tree {
+	tr := &Tree{
+		root: nil,
+		len:  0,
+	}
+	return tr
+}
+
+// Len return number of elements.
+func (tr *Tree) Len() int {
+	return tr.len
+}
+
+// Insert inserts the giving key and value as an Element and return.
+// Returns nil if key already exists.
+func (tr *Tree) Insert(k Key, v Value) Element {
+	var n *treeNode
+	p := tr.root
+	for p != nil {
+		flag := k.Compare(p.key)
+		if flag == -1 {
+			if p.left == nil {
+				n = tr.createNode(k, v, p)
+				p.left = n
+				break
+			}
+			p = p.left
+		} else if flag == 1 {
+			if p.right == nil {
+				n = tr.createNode(k, v, p)
+				p.right = n
+				break
+			}
+			p = p.right
+		} else {
+			// The key already exists. Not allowed duplicates.
+			return nil
+		}
+	}
+	if n == nil {
+		n = tr.createNode(k, v, p)
+	}
+
+	tr.insertBalance(n)
+	tr.len++
+
+	return n
+}
+
+// Delete removes and returns the Element of a given key.
+// Returns nil if not found.
+func (tr *Tree) Delete(k Key) Element {
+	d := tr.root
+	for d != nil {
+		flag := k.Compare(d.key)
+		if flag == -1 {
+			d = d.left
+		} else if flag == 1 {
+			d = d.right
+		} else {
+			break
+		}
+	}
+	if d == nil {
+		return nil
+	}
+
+	if d.left != nil && d.right != nil {
+		x := d.left
+		for x.right != nil {
+			x = x.right
+		}
+		tr.swap(d, x)
+		d = x
+	}
+
+	var c *treeNode
+
+	if d.left != nil {
+		c = d.left
+	} else {
+		c = d.right
+	}
+
+	if c != nil {
+		c.parent = d.parent
+	}
+
+	if d.parent == nil {
+		tr.root = c
+	} else if d.parent.left == d {
+		d.parent.left = c
+	} else {
+		d.parent.right = c
+	}
+
+	if d.color == black {
+		tr.deleteBalance(c, d.parent)
+	}
+
+	d.left = nil
+	d.right = nil
+	d.parent = nil
+	d.color = -1
+
+	tr.len--
+	return d
+}
+
+// Search returns the Element of a given key.
+// Returns nil if not found.
+func (tr *Tree) Search(k Key) Element {
+	p := tr.root
+	for p != nil {
+		flag := k.Compare(p.key)
+		if flag == -1 {
+			p = p.left
+		} else if flag == 1 {
+			p = p.right
+		} else {
+			return p
+		}
+	}
+	return nil
+}
+
+// Iter return an Iterator, it's a wrap for bs.Iterator.
+func (tr *Tree) Iter(start Key, boundary Key) container.Iterator {
+	return bs.NewIterator(tr.root, start, boundary)
+}
+
+func (tr *Tree) swap(n1, n2 *treeNode) {
+	n1.key, n2.key = n2.key, n1.key
+	n1.value, n2.value = n2.value, n1.value
+}
+
+func (tr *Tree) insertBalance(n *treeNode) {
+	if n.parent == nil {
+		n.color = black
+		tr.root = n
+		return
+	}
+	if n.parent.color == black {
+		return
+	}
+
+	var (
+		p, g, u *treeNode
+	)
+
+	p = n.parent
+	g = n.parent.parent
+
+	if g.left == p {
+		u = g.right
+	} else {
+		u = g.left
+	}
+
+	if u != nil && u.color == red {
+		g.color = red
+		p.color = black
+		u.color = black
+		tr.insertBalance(g)
+		return
+	}
+
+	if g.left == p {
+		if p.right == n {
+			tr.leftRotate(p)
+			p = g.left
+		}
+		g.color = red
+		p.color = black
+		tr.rightRotate(g)
+	} else {
+		if p.left == n {
+			tr.rightRotate(p)
+			p = g.right
+		}
+		g.color = red
+		p.color = black
+		tr.leftRotate(g)
+	}
+}
+
+func (tr *Tree) deleteBalance(n *treeNode, p *treeNode) {
+	if n != nil && n.color == red {
+		n.color = black
+		return
+	}
+
+	if p == nil {
+		tr.root = n
+		return
+	}
+
+	var s *treeNode
+
+	if p.left == n {
+		s = p.right
+		if s.color == red {
+			s.color = black
+			p.color = red
+			tr.leftRotate(p)
+			s = p.right
+		}
+		if (s.left == nil || s.left.color == black) && (s.right == nil || s.right.color == black) {
+			s.color = red
+			tr.deleteBalance(p, p.parent)
+			return
+		}
+		if (s.left != nil && s.left.color == red) && (s.right == nil || s.right.color == black) {
+			s.color = red
+			s.left.color = black
+			tr.rightRotate(s)
+			s = p.right
+		}
+		if s.right != nil && s.right.color == red {
+			s.color = p.color
+			p.color = black
+			s.right.color = black
+			tr.leftRotate(p)
+		}
+	} else {
+		s = p.left
+		if s.color == red {
+			s.color = black
+			p.color = red
+			tr.rightRotate(p)
+			s = p.left
+		}
+		if (s.left == nil || s.left.color == black) && (s.right == nil || s.right.color == black) {
+			s.color = red
+			tr.deleteBalance(p, p.parent)
+			return
+		}
+		if (s.right != nil && s.right.color == red) && (s.left == nil || s.left.color == black) {
+			s.color = red
+			s.right.color = black
+			tr.leftRotate(s)
+			s = p.left
+		}
+		if s.left != nil && s.left.color == red {
+			s.color = p.color
+			p.color = black
+			s.left.color = black
+			tr.rightRotate(p)
+		}
+	}
+}
+
+func (tr *Tree) createNode(k Key, v Value, p *treeNode) *treeNode {
+	return &treeNode{
+		key:    k,
+		value:  v,
+		left:   nil,
+		right:  nil,
+		parent: p,
+		color:  red,
+	}
+}
+
+func (tr *Tree) leftRotate(n *treeNode) {
+	r := n.right
+	if r.left != nil {
+		r.left.parent = n
+	}
+
+	n.right = r.left
+	r.left = n
+
+	r.parent = n.parent
+	n.parent = r
+
+	if r.parent == nil {
+		tr.root = r
+	} else if r.parent.left == n {
+		r.parent.left = r
+	} else {
+		r.parent.right = r
+	}
+}
+
+func (tr *Tree) rightRotate(n *treeNode) {
+	l := n.left
+	if l.right != nil {
+		l.right.parent = n
+	}
+
+	n.left = l.right
+	l.right = n
+
+	l.parent = n.parent
+	n.parent = l
+
+	if l.parent == nil {
+		tr.root = l
+	} else if l.parent.left == n {
+		l.parent.left = l
+	} else {
+		l.parent.right = l
+	}
+}
---
+++ rb/rbtree_test.go
@@ -0,0 +1,248 @@
+package rb
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/yu31/gostructs/bs"
+	"github.com/yu31/gostructs/container"
+)
+
+func recurseCalculateNodeHeight(n *treeNode) int {
+	if n == nil {
+		return 0
+	}
+	lh := recurseCalculateNodeHeight(n.left)
+	rh := recurseCalculateNodeHeight(n.right)
+	if lh > rh {
+		return lh + 1
+	}
+	return rh + 1
+}
+
+func checkBalance(t *testing.T, n *treeNode) {
+	if n == nil {
+		return
+	}
+
+	checkBalance(t, n.left)
+	checkBalance(t, n.right)
+
+	// Check the node color.
+	if n.color == red {
+		//if n.parent != nil {
+		//	require.Equal(t, n.parent.color, black)
+		//}
+		require.NotNil(t, n.parent)
+		require.Equal(t, n.parent.color, black)
+		if n.left != nil && n.right != nil {
+			require.Equal(t, n.left.color, black)
+			require.Equal(t, n.right.color, black)
+		} else {
+			require.Nil(t, n.left)
+			require.Nil(t, n.right)
+		}
+	} else {
+		require.Equal(t, n.color, black)
+	}
+
+	if n.left != nil {
+		require.Equal(t, n.key.Compare(n.left.key), 1)
+	}
+	if n.right != nil {
+		require.Equal(t, n.key.Compare(n.right.key), -1)
+	}
+
+	// The height difference must not exceed one time.
+	lh := recurseCalculateNodeHeight(n.left)
+	rh := recurseCalculateNodeHeight(n.right)
+	if lh > rh {
+		require.LessOrEqual(t, lh-rh, lh)
+	} else {
+		require.LessOrEqual(t, rh-lh, rh)
+	}
+}
+
+func Test_Interface(t *testing.T) {
+	// Ensure the interface is implemented.
+	var node bs.TreeNode
+	var element container.Element
+	var ct container.Container
+	var it container.Iterator
+
+	node = &treeNode{}
+	_ = node
+	element = &treeNode{}
+	_ = element
+	ct = New()
+	_ = ct
+	it = ct.Iter(nil, nil)
+	_ = it
+}
+
+func TestNew(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr)
+	require.Nil(t, tr.root)
+	require.Equal(t, tr.len, 0)
+}
+
+func TestTree_createNode(t *testing.T) {
+	tr := New()
+
+	k := container.Int64(0xf)
+	v := 1024
+
+	n := tr.createNode(k, v, nil)
+	require.NotNil(t, n)
+	require.Equal(t, n.key.Compare(k), 0)
+	require.Equal(t, n.value, v)
+	require.Nil(t, n.left)
+	require.Nil(t, n.right)
+	require.Nil(t, n.parent)
+	require.Equal(t, n.color, red)
+}
+
+func TestTree_Insert(t *testing.T) {
+	tr := New()
+
+	require.NotNil(t, tr.Insert(container.Int(11), 1024))
+	require.Nil(t, tr.Insert(container.Int(11), 1023))
+	require.NotNil(t, tr.Insert(container.Int(33), nil))
+	require.Nil(t, tr.Insert(container.Int(33), nil))
+	require.NotNil(t, tr.Insert(container.Int(22), nil))
+	require.Nil(t, tr.Insert(container.Int(22), nil))
+}
+
+func TestTree_Delete(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr.Insert(container.Int(11), 1021))
+	require.NotNil(t, tr.Insert(container.Int(22), 1022))
+	require.NotNil(t, tr.Insert(container.Int(33), 1023))
+
+	element := tr.Delete(container.Int(11))
+	require.NotNil(t, element)
+	require.Equal(t, element.Key().Compare(container.Int(11)), 0)
+	require.Equal(t, element.Value(), 1021)
+	require.Nil(t, element.(*treeNode).left)
+	require.Nil(t, element.(*treeNode).right)
+	require.Nil(t, element.(*treeNode).parent)
+	require.Equal(t, element.(*treeNode).color, int8(-1))
+	require.Nil(t, element.(TreeNode).Left())
+	require.Nil(t, element.(TreeNode).Left())
+	require.Nil(t, tr.Delete(container.Int(11)))
+
+	require.NotNil(t, tr.Delete(container.Int(22)))
+	require.Nil(t, tr.Delete(container.Int(22)))
+	require.NotNil(t, tr.Delete(container.Int(33)))
+	require.Nil(t, tr.Delete(container.Int(33)))
+
+	// Try to delete key not exists.
+	require.Nil(t, tr.Delete(container.Int(1024)))
+}
+
+func TestTree_Search(t *testing.T) {
+	tr := New()
+	require.NotNil(t, tr.Insert(container.Int(11), 1021))
+	require.NotNil(t, tr.Insert(container.Int(22), 1022))
+	require.NotNil(t, tr.Insert(container.Int(33), 1023))
+
+	require.Equal(t, tr.Search(container.Int(11)).Key().Compare(container.Int(11)), 0)
+	require.Equal(t, tr.Search(container.Int(11)).Value(), 1021)
+	require.Equal(t, tr.Search(container.Int(22)).Key().Compare(container.Int(22)), 0)
+	require.Equal(t, tr.Search(container.Int(22)).Value(), 1022)
+	require.Equal(t, tr.Search(container.Int(33)).Key().Compare(container.Int(33)), 0)
+	require.Equal(t, tr.Search(container.Int(33)).Value(), 1023)
+
+	// Try to search key not exists.
+	require.Nil(t, tr.Search(container.Int(1024)))
+}
+
+func TestTree_Len(t *testing.T) {
+	tr := New()
+
+	require.NotNil(t, tr.Insert(container.Int(12), 1))
+	require.NotNil(t, tr.Insert(container.Int(18), 1))
+	require.NotNil(t, tr.Insert(container.Int(33), 1))
+
+	// Insert duplicate key.
+	require.Nil(t, tr.Insert(container.Int(12), 1))
+	require.Nil(t, tr.Insert(container.Int(18), 1))
+	require.Nil(t, tr.Insert(container.Int(33), 1))
+
+	require.Equal(t, tr.Len(), 3)
+
+	require.NotNil(t, tr.Delete(container.Int(18)))
+	require.Nil(t, tr.Delete(container.Int(18)))
+
+	// Delete a not exist key.
+	require.Nil(t, tr.Delete(container.Int(1024)))
+
+	require.Equal(t, tr.Len(), 2)
+}
+
+func TestTree(t *testing.T) {
+	tr := New()
+
+	length := 257
+	maxKey := length * 100
+	keys := make([]container.Int64, length)
+
+	for x := 0; x < 2; x++ {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		// insert
+		for i := 0; i < length; i++ {
+			for {
+				k := container.Int64(r.Intn(maxKey) + 1)
+				if tr.Insert(k, int64(k*2+1)) != nil {
+					require.Nil(t, tr.Insert(k, int64(k*2+1)))
+					keys[i] = k
+					break
+				}
+			}
+			require.Equal(t, tr.root.color, black)
+			checkBalance(t, tr.root)
+			require.Equal(t, tr.Len(), i+1)
+		}
+
+		require.Equal(t, tr.Len(), length)
+
+		// boundary
+		for _, k := range []container.Int64{0, 0xfffffff} {
+			require.NotNil(t, tr.Insert(k, k))
+			require.Nil(t, tr.Insert(k, k))
+			require.NotNil(t, tr.Search(k))
+			require.Equal(t, tr.Search(k).Value(), k)
+			require.NotNil(t, tr.Delete(k))
+			require.Nil(t, tr.Delete(k))
+		}
+
+		// search
+		for i := 0; i < length; i++ {
+			element := tr.Search(keys[i])
+			require.NotNil(t, element)
+			require.Equal(t, element.Key().Compare(keys[i]), 0)
+			require.Equal(t, element.Value(), int64(keys[i]*2+1))
+		}
+
+		// delete
+		for i := 0; i < length; i++ {
+			require.NotNil(t, tr.Delete(keys[i]))
+			require.Nil(t, tr.Delete(keys[i]))
+			require.Nil(t, tr.Search(keys[i]))
+
+			if tr.root != nil {
+				require.Equal(t, tr.root.color, black)
+			}
+
+			checkBalance(t, tr.root)
+			require.Equal(t, tr.Len(), length-i-1)
+		}
+
+		require.Nil(t, tr.root)
+		require.Equal(t, tr.Len(), 0)
+	}
+}
