6531a61acceabf5b9666916b0d2b9258ada93837
wuyu <wuyu@yunify.com>
2020-12-12 21:53:31 +0800

    Add package container that moved from gohelper

---
+++ container/comparator.go
@@ -0,0 +1,240 @@
+// Copyright (c) 2020, Yu Wu <yu.771991@gmail.com> All rights reserved.
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package container
+
+import (
+	"bytes"
+	"time"
+)
+
+// Comparator defines an interface of wraps any data-type of comparison two elements.
+type Comparator interface {
+	// Compare returns the comparison result with another Comparator.
+	// The result will be 0 if source == target, -1 if source < target, and 1 if source > target.
+	Compare(target Comparator) int
+}
+
+// Wrapper for builtin type string.
+type String string
+
+func (k1 String) Compare(target Comparator) int {
+	k2 := target.(String)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type byte.
+type Byte byte
+
+func (k1 Byte) Compare(target Comparator) int {
+	k2 := target.(Byte)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type rune.
+type Rune rune
+
+func (k1 Rune) Compare(target Comparator) int {
+	k2 := target.(Rune)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type int.
+type Int int
+
+func (k1 Int) Compare(target Comparator) int {
+	k2 := target.(Int)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type int8.
+type Int8 int8
+
+func (k1 Int8) Compare(target Comparator) int {
+	k2 := target.(Int8)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type int16.
+type Int16 int16
+
+func (k1 Int16) Compare(target Comparator) int {
+	k2 := target.(Int16)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type int32.
+type Int32 int32
+
+func (k1 Int32) Compare(target Comparator) int {
+	k2 := target.(Int32)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type int64.
+type Int64 int64
+
+func (k1 Int64) Compare(target Comparator) int {
+	k2 := target.(Int64)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type uint.
+type Uint uint
+
+func (k1 Uint) Compare(target Comparator) int {
+	k2 := target.(Uint)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type uint8.
+type Uint8 uint8
+
+func (k1 Uint8) Compare(target Comparator) int {
+	k2 := target.(Uint8)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type uint16.
+type Uint16 uint16
+
+func (k1 Uint16) Compare(target Comparator) int {
+	k2 := target.(Uint16)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type uint32.
+type Uint32 uint32
+
+func (k1 Uint32) Compare(target Comparator) int {
+	k2 := target.(Uint32)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for builtin type uint33.
+type Uint64 uint64
+
+func (k1 Uint64) Compare(target Comparator) int {
+	k2 := target.(Uint64)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for type byte slice.
+type Bytes []byte
+
+func (k1 Bytes) Compare(target Comparator) int {
+	k2 := target.(Bytes)
+	return bytes.Compare(k1, k2)
+}
+
+// Wrapper for type time.Duration.
+type Duration time.Duration
+
+func (k1 Duration) Compare(target Comparator) int {
+	k2 := target.(Duration)
+	if k1 < k2 {
+		return -1
+	}
+	if k1 > k2 {
+		return 1
+	}
+	return 0
+}
+
+// Wrapper for type time.Time.
+type Time time.Time
+
+func (k1 Time) Compare(target Comparator) int {
+	k2 := target.(Time)
+
+	t1 := time.Time(k1)
+	t2 := time.Time(k2)
+
+	if t1.Before(t2) {
+		return -1
+	}
+	if t1.After(t2) {
+		return 1
+	}
+	return 0
+}
---
+++ container/container.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2020, Yu Wu <yu.771991@gmail.com> All rights reserved.
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+package container
+
+// Key represents high-level Key type.
+type Key = Comparator
+
+// Value represents high-level Value type.
+type Value interface{}
+
+// Element is an element of a Container.
+type Element interface {
+	// Key returns the key that stored with this element.
+	Key() Key
+
+	// Value returns the value that stored with this element.
+	Value() Value
+}
+
+// Container defines an data container interface.
+type Container interface {
+	// Len returns the number of elements.
+	Len() int
+
+	// Insert inserts the giving key and value as an Element and return.
+	// Returns nil if key already exists.
+	Insert(k Key, v Value) (element Element)
+
+	// Delete removes and returns the Element of a given key.
+	// Returns nil if not found.
+	Delete(k Key) (element Element)
+
+	// Search returns the Element of a given key.
+	// Returns nil if not found.
+	Search(k Key) (element Element)
+
+	// Iter creates an iterator to iteration return element.
+	//
+	// The element range is start <= x < boundary.
+	// The element will return from the beginning if start is nil,
+	// And return until the end if the boundary is nil.
+	Iter(start Key, boundary Key) Iterator
+}
+
+// The iterator is an interface for iteration return element.
+type Iterator interface {
+	// Valid represents whether to have more elements in the Iterator.
+	Valid() bool
+
+	// Next returns a Element and moved the iterator to the next Element.
+	// Returns nil if no more elements.
+	Next() Element
+}
